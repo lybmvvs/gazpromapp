321fce7f51a18921bfd988f8d428dc71d9533f72
lybmvvs <stik9543@gmail.com>
2021-11-09 02:15:37 +0300

    refactored code in main.py pep8

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-
+import sys
+import datetime
+import pandas as pd
 from PyQt5 import QtCore, QtGui, QtWidgets
 from window1 import Ui_firstwindow
 from window2 import Ui_secondwindow
-import pandas as pd
-import datetime
-import sys
+
+
 app = QtWidgets.QApplication(sys.argv)
-firstwindow = QtWidgets.QWidget()
+first_window = QtWidgets.QWidget()
 ui = Ui_firstwindow()
-ui.setupUi(firstwindow)
-firstwindow.show()
-def OpenSecondWindow():
-    global secondwindow
-    secondwindow = QtWidgets.QWidget()
+ui.setupUi(first_window)
+first_window.show()
+
+
+def open_second_window():
+    global second_window
+    second_window = QtWidgets.QWidget()
     ui = Ui_secondwindow()
-    ui.setupUi(secondwindow)
-    firstwindow.close()
-    secondwindow.show()
+    ui.setupUi(second_window)
+    first_window.close()
+    second_window.show()
+
+
 def im_file():
     file_name = ui.lineEdit.text()
 
     print(file_name)
+
+
 ui.pushButton.clicked.connect(im_file)
-ui.pushButton.clicked.connect(OpenSecondWindow)
+ui.pushButton.clicked.connect(open_second_window)
 sys.exit(app.exec_())
